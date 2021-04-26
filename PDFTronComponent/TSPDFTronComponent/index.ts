555677093f411f6144f23f3fee343c1973df5881
mike-mh <0x4d464d48@gmail.com>
2021-04-26 13:46:33 -0700

    Added WebViewer code

--- PDFTronComponent/TSPDFTronComponent/index.ts
+++ PDFTronComponent/TSPDFTronComponent/index.ts
@@ -1,53 +1,54 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import WebViewer from '@pdftron/webviewer';
+export class TSPDFTronComponent
+  implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+  private _notifyOutputChanged: () => void;
+  // This element contains all elements of our code component example
+  private _container: HTMLDivElement;
+  // reference to Power Apps component framework Context object
+  private _context: ComponentFramework.Context<IInputs>;
+  // Event Handler 'refreshData' reference
+  private _refreshData: EventListenerOrEventListenerObject;
 
-export class TSPDFTronComponent implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+  constructor() {}
 
-	/**
-	 * Empty constructor.
-	 */
-	constructor()
-	{
+  public init(
+    context: ComponentFramework.Context<IInputs>,
+    notifyOutputChanged: () => void,
+    state: ComponentFramework.Dictionary,
+    container: HTMLDivElement
+  ) {
+    this._context = context;
+    this._container = document.createElement("div");
+    this._notifyOutputChanged = notifyOutputChanged;
+    this._refreshData = this.refreshData.bind(this);
+    // Need to manually insert script tag for WebViewer CDN
+	const webViewreScript = document.createElement('script');
+    webViewreScript.src= 'REPLACE_WITH_CDN_URL';
+    document.body.appendChild(webViewreScript);
 
-	}
+	// Add WebViewer div to the DOM
+	const viewerNode = document.createElement('div');
+	viewerNode.style.width = '710px';
+	viewerNode.style.height = '500px';
+	viewerNode.id = 'viewer';
 
-	/**
-	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-	 * Data-set values are not initialized here, use updateView.
-	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-	 */
-	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
-	{
-		// Add control initialization code
-	}
+	this._container.appendChild(viewerNode);
+	container.appendChild(this._container);
+	
+	(WebViewer as any)({
+		path: 'REPLACE_WITH_CDN_PATH_TO_LIB',
+ 		config: 'REPLACE_WITH_TO_CDN_TO_YOUR_CONFIG_FILE',
+	}, viewerNode)
+  }
 
+  public refreshData(evt: Event): void {
+    this._notifyOutputChanged();
+  }
 
-	/**
-	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-	 */
-	public updateView(context: ComponentFramework.Context<IInputs>): void
-	{
-		// Add code to update control view
-	}
+  public updateView(context: ComponentFramework.Context<IInputs>): void {
+  }
 
-	/** 
-	 * It is called by the framework prior to a control receiving new data. 
-	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
-	 */
-	public getOutputs(): IOutputs
-	{
-		return {};
-	}
-
-	/** 
-	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-	 * i.e. cancelling any pending remote calls, removing listeners, etc.
-	 */
-	public destroy(): void
-	{
-		// Add code to cleanup control if necessary
-	}
+  public destroy() {
+  }
 }
